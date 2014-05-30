b2d879b87384384f0074a53f8e23b9081a488968
RSid <sm1th303@gmail.com>
2014-05-30 09:43:26 -0400

    all notes in a 4/4 measure  enterable by user, next step is to make variable timesig and/or one-by-one note entry supported

--- server.rb
+++ server.rb
@@ -8,6 +8,10 @@ enable :sessions
 get '/' do
 
   @note=session[:note]
+  @note2=session[:note2]
+  @note3=session[:note3]
+  @note4=session[:note4]
+  @timesig=session[:timesig]
 
   erb :index
 end
@@ -15,6 +19,10 @@ end
 post '/' do
   session[:timesig]="#{params["timesignature"]}"
   session[:note]="#{params["note"]}/#{session[:timesig]}"
+  session[:note2]="#{params["note2"]}/#{session[:timesig]}"
+  session[:note3]="#{params["note3"]}/#{session[:timesig]}"
+  session[:note4]="#{params["note4"]}/#{session[:timesig]}"
+
 
   redirect '/'
 end
