093cac7fd8313f5e4b10e0758c7b75cf3ec6bfdc
RSid <sm1th303@gmail.com>
2014-05-30 10:55:43 -0400

    drafted methods for determining whether the user has entered a note or a chord - currently can process individual notes and processes chords as individual notes for now, until I've finished implementing the 'chordify' method'

--- server.rb
+++ server.rb
@@ -12,6 +12,35 @@ def note_validator (note_array)
   end
 end
 
+def chordify_note (note_string,timesig_string)
+  note=note_string[0]
+  note_array=[(note/timesig_string)]
+
+  if note_string.include? "min"
+    #minor chord, 3/4
+    #need to figure out good way to count alphabet in half-steps
+    note_array<<((note.next +  "\#")/timesig_string)
+    note_array<<(note.next/timesig_string)
+
+  else
+    #major chord, 4/3, note to people that it defaults to major
+    note_array<<(note.next/timesig_string)
+    note_array<<((note.next +  "\#")/timesig_string)
+
+  end
+
+end
+
+def chord_or_note (note_string,timesig_string)
+  return_note=""
+  if note_string.include? "chord"
+    return_note=note_string[0] + '/' + timesig_string #chordified note eventually, just testing now
+  else
+    return_note=note_string + '/' + timesig_string
+  end
+  return_note
+end
+
 
 get '/' do
 
@@ -23,11 +52,11 @@ get '/' do
 end
 
 post '/' do
-  session[:timesig]="#{params["timesignature"]}"
-  session[:note]="#{params["note"]}/#{session[:timesig]}"
-  session[:note2]="#{params["note2"]}/#{session[:timesig]}"
-  session[:note3]="#{params["note3"]}/#{session[:timesig]}"
-  session[:note4]="#{params["note4"]}/#{session[:timesig]}"
+  session[:timesig] = "#{params["timesignature"]}"
+  session[:note] = chord_or_note(params["note"],session[:timesig])
+  session[:note2] = chord_or_note(params["note2"],session[:timesig])
+  session[:note3] = chord_or_note(params["note3"],session[:timesig])
+  session[:note4] = chord_or_note(params["note4"],session[:timesig])
 
 
   redirect '/'
