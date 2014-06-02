6b29dde03c9263ed9a7f1028db510e3c2f32c807
RSid <sm1th303@gmail.com>
2014-06-02 13:57:14 -0400

    Notes & chords can be added dynamically, based on the number entered by user

--- server.rb
+++ server.rb
@@ -5,38 +5,33 @@ require 'shotgun'
 enable :sessions
 
 def note_validator (note_array)
-  note_array.each do |note|
-
-    if note.is_a?(String)
-
-      unless ("a".."g").include? note[0]
-       return false
-      end
-
-    elsif note.is_a?(Array)
-
-      note.each do |sub_note|
-
-        unless ("a".."g").include? sub_note[0]
-          return false
+  if note_array != nil
+    note_array.each do |note|
+      if note.is_a?(String)
+        unless ("a".."g").include? note[0]
+         return false
         end
-
+      elsif note.is_a?(Array)
+        note.each do |sub_note|
+          unless ("a".."g").include? sub_note[0]
+            return false
+          end
+        end
       end
-
     end
   end
 
 end
 
-def note_step(note_and_vertpos) #interval
+def note_step(note_and_vertpos) #interval?
   alphabet = ['c','d','e','f','g','a','b']
 
-  note=note_and_vertpos[0]
-  vertpos=note_and_vertpos[-1].to_i
+  note = note_and_vertpos[0]
+  vertpos = note_and_vertpos[-1].to_i
 
-  if (alphabet.index(note) + 2)<alphabet.length
+  if (alphabet.index(note) + 2) < alphabet.length
     (alphabet[(alphabet.index(note) + 2)]) + '/' + vertpos.to_s
-  elsif (alphabet.index(note) + 1)<alphabet.length
+  elsif (alphabet.index(note) + 1) < alphabet.length
     alphabet[0] + '/' + (vertpos+1).to_s
   else
     alphabet[1] + '/' + (vertpos+1).to_s
@@ -44,10 +39,10 @@ def note_step(note_and_vertpos) #interval
 end
 
 def chordify_note (note_string,vertpos)
-  note=note_string[0]
-  full_note=note+'/' +vertpos
-  note_array=[full_note]
-  alphabet=('a'..'g').to_a
+  note = note_string[0]
+  full_note = note + '/' + vertpos
+  note_array = [full_note]
+  alphabet = ('a'..'g').to_a
 
   middle_note =  note_step(full_note)
   last_note =  note_step(middle_note)
@@ -64,37 +59,35 @@ def chordify_note (note_string,vertpos)
     note_array << (middle_note)
     note_array << (last_note)
 
-    # alphabet[((alphabet.index(note)) + 4 )]
   end
   note_array
-  #binding.pry
 end
 
-def chord_or_note (note_string,vertpos)
-  if note_string.include? "chord"
-    return_note=chordify_note(note_string,vertpos)
-  else
-    return_note=note_string + '/' + vertpos
-  end
-  return_note
+def chord_or_note (note_array,vertpos)
+  final_note_array = []
+    note_array.each do |note_string|
+      if note_string.include? "chord"
+        final_note_array << chordify_note(note_string,vertpos)
+      else
+        final_note_array << [note_string + '/' + vertpos]
+      end
+    end
+  final_note_array
 end
 
 
 get '/' do
-  @note_array=note_validator([session[:note],session[:note2],session[:note3],session[:note4]])
-  #@note_array.delete(nil)
-  @timesig=session[:timesig]
+  @note_array = note_validator(session[:notes])
+  @numbeats = session[:numbeats]
 
   erb :index
 end
 
 post '/' do
   session.clear
-  session[:timesig] = "#{params["timesignature"]}"
-  session[:note] = chord_or_note(params["note"],"4")
-  session[:note2] = chord_or_note(params["note2"],"4")
-  session[:note3] = chord_or_note(params["note3"],"4")
-  session[:note4] = chord_or_note(params["note4"],"4")
+  session[:numbeats] = "#{params["numbeats"]}"
+  note_array = (params["notes"]).split(',')
+  session[:notes] = chord_or_note(note_array,"4")
 
 
   redirect '/'
